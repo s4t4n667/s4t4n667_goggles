09d90785af891708d37a5f7d1a74b691e49cacc8
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2024-12-06 16:34:18 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with Durty Cloth Tool v3.1.0.3 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_gogglespack_shop.meta',
+  'mp_f_freemode_01_mp_f_gogglespack_shop.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_gogglespack_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_gogglespack_shop.meta'
